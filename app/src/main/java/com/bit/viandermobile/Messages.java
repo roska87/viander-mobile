9f1326cc61cbf992c943f4cc09761234190f758a
GabrielB47 <agustinborba@hotmail.com>
2021-04-11 20:28:39 -0300

    Button

--- app/src/main/java/com/bit/viandermobile/Messages.java
+++ app/src/main/java/com/bit/viandermobile/Messages.java
@@ -1,4 +1,10 @@
 package com.bit.viandermobile;
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class Messages {
+    public static void message (Context context, String msg){
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
 }
