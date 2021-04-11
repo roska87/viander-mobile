b92792f3b3ecb46990ed7fd982e3fa2a5d4733f8
GabrielB47 <agustinborba@hotmail.com>
2021-04-11 06:54:35 -0300

    User details activity

---
+++ app/src/main/java/com/bit/viandermobile/SpecsActivity.java
@@ -0,0 +1,14 @@
+package com.bit.viandermobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpecsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specs);
+    }
+}
