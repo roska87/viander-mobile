9f12ab155694f63555edd50e5e329196e8384b9b
GabrielB47 <agustinborba@hotmail.com>
2021-04-18 11:53:55 -0300

    Home test

--- app/src/main/java/com/bit/viandermobile/HomeActivity.java
+++ app/src/main/java/com/bit/viandermobile/HomeActivity.java
@@ -7,16 +7,22 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.transition.Slide;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.bit.viandermobile.domain.PostDto;
@@ -48,12 +54,14 @@ public class HomeActivity extends AppCompatActivity {
 
     // variable for ViewPager
     private ViewPager2 viewPager2;
+    private Handler sliderHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // Slider ViewPager
         viewPager2 = findViewById(R.id.viewPagerImageSlider);
         drawerLayout = findViewById(R.id.activityHome);
 
@@ -64,6 +72,32 @@ public class HomeActivity extends AppCompatActivity {
         sliderItems.add(new SliderItem(R.drawable.canelones_home_temporal));
 
         viewPager2.setAdapter(new SliderAdapter(sliderItems, viewPager2));
+
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback(){
+            @Override
+            public void onPageSelected (int position){
+                super.onPageSelected(position);
+                sliderHandler.removeCallbacks(sliderRunnable);
+                sliderHandler.postDelayed(sliderRunnable, 3000);
+            }
+        });
+        //
 
         vianderViewModel = new ViewModelProvider(this, new VianderFactory(getApplication())).get(VianderViewModel.class);
         sessionViewModel = new ViewModelProvider(this, new SessionFactory(getApplication())).get(SessionViewModel.class);
@@ -104,6 +138,15 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
+    //Slider ViewPager
+    private Runnable sliderRunnable = new Runnable(){
+        @Override
+        public void run(){
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
+        }
+    };
+
+    //DrwaerLayout
     public void clickMenu(View view){
         openDrawer(drawerLayout);
     }
@@ -195,7 +238,18 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+
+        //DrawerLayout
         closeDrawer(drawerLayout);
+        
+        //Slider ViewPager
+        sliderHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sliderHandler.postDelayed(sliderRunnable, 3000);
     }
 
 
--- app/src/main/java/com/bit/viandermobile/SliderAdapter.java
+++ app/src/main/java/com/bit/viandermobile/SliderAdapter.java
@@ -12,7 +12,7 @@ import com.makeramen.roundedimageview.RoundedImageView;
 
 import java.util.List;
 
-public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderViewHolder>{
+public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderViewHolder> {
 
     private List<SliderItem> sliderItems;
     private ViewPager2 viewPager2;
@@ -26,25 +26,29 @@ public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderView
     @Override
     public SliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new SliderViewHolder(
-            LayoutInflater.from(parent.getContext()).inflate(
-                    R.layout.slide_item_container,
-                    parent,
-                    false
-            )
+                LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.slide_item_container,
+                        parent,
+                        false
+                )
         );
     }
 
     @Override
     public void onBindViewHolder(@NonNull SliderViewHolder holder, int position) {
         holder.setImage(sliderItems.get(position));
+        if (position == sliderItems.size() - 2) {
+            viewPager2.post(runnable);
+        }
     }
+
 
     @Override
     public int getItemCount() {
         return sliderItems.size();
     }
 
-    class SliderViewHolder extends RecyclerView.ViewHolder{
+    class SliderViewHolder extends RecyclerView.ViewHolder {
         private RoundedImageView imageView;
 
         SliderViewHolder(@NonNull View itemView) {
@@ -52,9 +56,17 @@ public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderView
             imageView = itemView.findViewById(R.id.imageSlide);
         }
 
-        void setImage(SliderItem sliderItem){
+        void setImage(SliderItem sliderItem) {
             imageView.setImageResource(sliderItem.getImage());
         }
 
     }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            sliderItems.addAll(sliderItems);
+            notifyDataSetChanged();
+        }
+    };
 }
